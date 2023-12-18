08a45b0d21cc524dba2853ae7675905d411c841d
Anant Shyam <anantshyam19@gmail.com>
2023-12-17 22:16:01 -0500

    add argument parser and revise model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import re
 import torch
 import gensim
-from nltk.tokenize import sent_tokenize, word_tokenize
+import argparse
 
 
 class Main:
@@ -27,7 +27,7 @@ class Main:
 
         self.train()
         print("Printing Test Prediction")
-        print(self.test(["machine", "learning"]))
+        print(self.test(["Python", "programming"]))
 
     def test(self, sentence):
         self.model.eval()
@@ -60,9 +60,6 @@ class Main:
                 # print(f"Prediction {prediction}")
                 true_label = self.class_to_number[self.correct_outputs[i]]
                 # print(f"True Label{true_label}")
-
-                # print(prediction.shape)
-                # print(torch.tensor(true_label).shape)
                 loss = self.loss_function(torch.tensor([float(prediction)], requires_grad=True).float(), torch.tensor([float(true_label)], requires_grad=True).float())
 
                 self.optimizer.zero_grad()
@@ -91,7 +88,7 @@ class Model(torch.nn.Module):
 
 class Dataset:
 
-    def __init__(self, subjects=['CS'], context_size=3, embedding_dim=10):
+    def __init__(self, subjects=['CS'], context_size=3):
 
         self.data = data_processing.process_data(subjects)
         self.data_size = min([len(self.data[i]) for i in range(len(self.data))])
@@ -102,39 +99,47 @@ class Dataset:
 
         self.dataset, self.sentences = self.build_dataset()
         self.vocabulary, self.classes = self.inspect_dataset()
-        self.embedding_dim = embedding_dim
 
     def build_dataset(self):
         dataset = []
         common_query_starts = ["Tell me more about", "Want to learn about", "I want to explore"]
         common_response_starts = ["You should consider taking", "I would definitely recommend"]
 
-        num_epochs = 2000
+        num_epochs = 100
         for _ in tqdm(range(num_epochs)):
             query_start = random.choice(common_query_starts)
             response_start = random.choice(common_response_starts)
-            random_subject = random.choice(self.data)
-            random_course = random.choice(list(random_subject.items()))[0]
 
-            words, word = [], ""
+            # random_subject = random.choice(self.data)
+            # print(random_subject)
+            # random_course = random.choice(list(random_subject.items()))[0]
+            # print(list(random_subject.items()))
+            # print(random_course)
 
-            for i in random_subject[random_course][0]:
-                if i == ' ':
-                    words.append(word)
-                    word = ''
-                else:
-                    word = word + i
-            if word != '':
-                words.append(word)
+            # loop over all the subjects
+            for random_subject in self.data:
+                for course in list(random_subject.items()):
 
-            if len(words) <= self.context_size:
-                continue
+                    random_course = course[0]
+                    words, word = [], ""
 
-            # get random word
-            representative_word_idx = random.randint(0, len(words) - self.context_size)
-            random_course = "".join((e for e in random_course if e != " "))
-            # dataset.append({f"{query_start} {' '.join((e for e in [words[i] for i in range(representative_word_idx, representative_word_idx + 3)]))}": f"{response_start} {random_course}"})
-            dataset.append({f"{' '.join((e for e in [words[i] for i in range(representative_word_idx,representative_word_idx + self.context_size)]))}": f"{random_course}"})
+                    for i in random_subject[random_course][0]:
+                        if i == ' ':
+                            words.append(word)
+                            word = ''
+                        else:
+                            word = word + i
+                    if word != '':
+                        words.append(word)
+
+                    if len(words) <= self.context_size:
+                        continue
+
+                    # get random word
+                    representative_word_idx = random.randint(0, len(words) - self.context_size)
+                    random_course = "".join((e for e in random_course if e != " "))
+                    # dataset.append({f"{query_start} {' '.join((e for e in [words[i] for i in range(representative_word_idx, representative_word_idx + 3)]))}": f"{response_start} {random_course}"})
+                    dataset.append({f"{' '.join((e for e in [words[i] for i in range(representative_word_idx,representative_word_idx + self.context_size)]))}": f"{random_course}"})
 
         sentences = []
         for example in dataset:
@@ -183,12 +188,21 @@ class Dataset:
 
 
 if __name__ == "__main__":
-    sentence_data = Dataset(context_size=8)
+    parser = argparse.ArgumentParser(description="Process User Command Line Arguments")
+    parser.add_argument("subjects")
+    parser.add_argument("query")
+    arguments = parser.parse_args()
+
+    user_subjects = arguments.subjects
+
+    sentence_data = Dataset(subjects=[user_subjects.strip()], context_size=8)
+    # print(sentence_data.dataset)
     # print(model.dataset)
     # print(model.sentences)
     # print(sentence_data.classes)
+
     model = Model(sentence_data)
-    print(model.sentences)
+    # print(model.sentences)
     print(model.num_classes)
     main = Main(model)
     print(main.class_to_number)
